fd570fb5b748e64cf2511c1dae971e68194e1e85
hermanb <bideherman@yahoo.com>
2023-04-20 18:21:04 -0700

    updated m07

--- code/module-07/m07-start/module07_loan-programs.ts
+++ code/module-07/m07-start/module07_loan-programs.ts
@@ -1,21 +1,14 @@
 /*  TODO Add the import statement. */
-interface Loan {
-    principle: number;
-    interestRate: number;
-}
-
-interface ConventionalLoan extends Loan {
-    months: number;
-}
+import {Loan, ConventionalLoan } from './module07_loans'
 
 /*  TODO Update the calculateInterestOnlyLoanPayment function. */
-const calculateInterestOnlyLoanPayment = (loanTerms: Loan): string => {
+export const calculateInterestOnlyLoanPayment = (loanTerms: Loan): string => {
     let payment: number;
     payment = loanTerms.principle * calculateInterestRate(loanTerms.interestRate);
     return 'The interest only loan payment is ' + payment.toFixed(2);
 }
 /*  TODO Update the calculateConventionalLoanPayment function. */
-function calculateConventionalLoanPayment(loanTerms: ConventionalLoan): string {
+export function calculateConventionalLoanPayment(loanTerms: ConventionalLoan): string {
     let interest: number = calculateInterestRate(loanTerms.interestRate);
     let payment: number;
     payment = loanTerms.principle * interest / (1 - (Math.pow(1/(1 + interest), loanTerms.months)));
